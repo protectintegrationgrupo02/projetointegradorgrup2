6d1db01bd42572e41978488c9fa339ec9ea07c7d
JardsonLucena <lucena.jardson@gmail.com>
2022-07-09 02:01:49 -0300

    att git

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/sobre', (req, res) => {
+  res.render('sobre');
+});
+
+app.get('/agenda', (req, res) => {
+  res.render('agenda');
+});
+
+app.get('/planos', (req, res) => {
+  res.render('planos');
+
+});
+
+app.get('/indextelainicial', (req, res) => {
+  res.render('indextelainicial');
+
+});
+
+app.get('/servico', (req, res) => {
+  res.render('servico');
+
+});
+
+// region ADM
+
+app.use('/admm', express.static(__dirname + '/System ADM BArbearia/template'))
+
+app.get('/adm/cadastro', (req, res) => {
+  res.render('admhtml/cadastroadm');
+
+});
+
+app.get('/configconta', (req, res) => {
+  res.render('configconta');
+
+});
+
+app.get('/configgeral', (req, res) => {
+  res.render('configgeral');
+
+});
+
+app.get('/lista', (req, res) => {
+  res.render('lista');
+
+});
+// endregion
+
+app.listen(PORT, () => console.info(`Server running on port ${PORT}`));
